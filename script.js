376f2ef6fecb51f34ba717545de574c9178f0064
Luaevente <ooochs.lukas.privat@gmail.com>
2025-12-10 15:06:36 +0100

    Email Spam-Sicher hinzugefügt und Kontaktformilar netlify angepasst

--- script.js
+++ script.js
@@ -15,3 +15,19 @@
         }, false)
     })
 })()
+
+// E-Mail Schutz für den Footer
+document.addEventListener('DOMContentLoaded', function() {
+    // Einstellungen für die E-Mail
+    var user = 'info';
+    var domain = 'bucher-isolierungen.de';
+    var email = user + '@' + domain;
+
+    // Suche ALLE Elemente mit der Klasse 'kontakt-email-placeholder'
+    var emailElements = document.querySelectorAll('.kontakt-email-placeholder');
+
+    // Gehe jedes gefundene Element durch und füge die E-Mail ein
+    emailElements.forEach(function(element) {
+        element.innerHTML = '<a href="mailto:' + email + '" class="text-light text-decoration-none">' + email + '</a>';
+    });
+});
